21fb6be616685b2c1a5cdd0158d8b73e06c6511b
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-06-30 18:00:28 +0530

    Routing fx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import Sigin from "./Components/Sigin";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sigin" element={<Sigin />} />
+          <Route path="/register" element={<register />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Sigin.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Sigin = () => {
+  return <div></div>;
+};
+
+export default Sigin;
