4a2dfa901c7ed6b36299f089a4b6669cac802c02
Saksham Raghuvanshi <86155054+saksham-raghuvanshi@users.noreply.github.com>
2023-06-30 18:37:34 +0530

    Login page done

--- src/Components/Sigin.js
+++ src/Components/Sigin.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Sigin = () => {
-  return <div>Login</div>;
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="title">Login</span>
+        <form>
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <button>Sign up</button>
+        </form>
+        <p>
+          Create a Account <Link to="/register">Register</Link>{" "}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Sigin;
